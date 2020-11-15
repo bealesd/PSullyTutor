d0952319c43b0a06620df9101425aa92aeade9af
David Beales <david.beales@ukho.gov.uk>
2020-11-15 20:18:36 +0000

    Facebook resizing plugin.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
+import { fromEvent } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +12,40 @@ export class AppComponent {
   title = 'psullytutor';
   colredBackground: boolean;
   fbHidden = false;
-  fbWidth = '250';
+  fbBig = false;
+  fbMedium = false;
+  fbSmall = false;
+
+  get isSmallShown(){
+    if(this.fbHidden === true)
+      return false;
+    if(this.fbSmall === false)
+      return  false;
+    return true;
+  }
+
+  get isMediumShown(){
+    if(this.fbHidden === true)
+      return false;
+    if(this.fbMedium === false)
+      return  false;
+    return true;
+  }
+
+  get isBigShown(){
+    if(this.fbHidden === true)
+      return false;
+    if(this.fbBig === false)
+      return  false;
+    return true;
+  }
 
   constructor(router: Router) {
+    this.resizeFbContainer();
+    
+    fromEvent(window, 'resize').pipe(debounceTime(300))
+      .subscribe(() => this.resizeFbContainer());
+      
     router.events.forEach((event) => {
       if (event instanceof NavigationStart) {
         if (['testimonials', 'home', ''].includes(event.url.split('/')[1]))
@@ -26,21 +59,19 @@ export class AppComponent {
           this.fbHidden = true;
       }
     });
-
-    this.resizeFbContainer();
   }
 
   resizeFbContainer() {
-    if (this.pageWidth > 1300)
-      this.fbWidth = '500';
-    else if (this.pageWidth > 1050)
-      this.fbWidth = '500';
-    else if (this.pageWidth > 750)
-      this.fbWidth = '500';
+    this.fbBig = false;
+    this.fbMedium = false;
+    this.fbSmall = false;
+
+    if (this.pageWidth > 700)
+      this.fbBig = true;
     else if (this.pageWidth > 500)
-      this.fbWidth = '250';
+      this.fbMedium = true;
     else if (this.pageWidth < 500)
-      this.fbWidth = '200';
+      this.fbSmall = true;
   }
 
   get pageWidth() {
