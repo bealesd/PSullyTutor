a6f43dc4ed00e42554c80ee097ba9d69e4e5e10b
Dave Beale <d beales>
2020-11-23 11:08:11 +0000

    Added fonts in assets.

--- README.md
+++ README.md
@@ -11,4 +11,4 @@ ng serve
 ng build --prod --output-path docs --base-href "/PSullyTutor/" 
 ng build --prod --output-path docs --base-href "https://www.psullyltd.com"
 
-ng build --prod --output-path docs --base-href "/"   N.B. add CNAME and 404.html, USE THIS ONE!
+ng build --prod --output-path docs --base-href "/"   N.B. dont delete CNAME, 404.html, robots.txt, sitemap.txt - USE THIS ONE!
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,32 +11,9 @@ import { debounceTime } from 'rxjs/operators';
 export class AppComponent {
   title = 'psullytutor';
   colredBackground: boolean;
-  fbHidden = false;
-  fbBig = false;
-  fbSmall = false;
-
-  get isSmallShown(){
-    if(this.fbHidden === true)
-      return false;
-    if(this.fbSmall === false)
-      return  false;
-    return true;
-  }
-
-  get isBigShown(){
-    if(this.fbHidden === true)
-      return false;
-    if(this.fbBig === false)
-      return  false;
-    return true;
-  }
+  isfbHidden = true;
 
   constructor(router: Router) {
-    this.resizeFbContainer();
-    
-    fromEvent(window, 'resize').pipe(debounceTime(300))
-      .subscribe(() => this.resizeFbContainer());
-      
     router.events.forEach((event) => {
       if (event instanceof NavigationStart) {
         if (['testimonials', 'home', ''].includes(event.url.split('/')[1]))
@@ -45,21 +22,11 @@ export class AppComponent {
           this.colredBackground = true;
 
         if (['contact'].includes(event.url.split('/')[1]))
-          this.fbHidden = false;
+          this.isfbHidden = false;
         else
-          this.fbHidden = true;
+          this.isfbHidden = true;
       }
     });
-  }
-
-  resizeFbContainer() {
-    this.fbBig = false;
-    this.fbSmall = false;
-
-    if (this.pageWidth > 700)
-      this.fbBig = true;
-    else if (this.pageWidth <= 700)
-      this.fbSmall = true;
   }
 
   get pageWidth() {
